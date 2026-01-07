0e1a5d38e7d7a2ea61673ade9f3d958cd6654fa7
bsomdatta <biswassomdatta100@gmail.com>
2026-01-07 10:01:34 +0530

    Initial commit

---
+++ server/models/User.js
@@ -0,0 +1,19 @@
+import momgoose from 'mongoose';
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+
+}, { timestamps: true })
+
+
+UserSchema.methods.comparePassword = function () {
+    return bcrypt.compareSync(password, this.password)
+}
+
+const User = mongoose.model("User", UserSchema)
+
+export default User;
